d426cae4bddd6e486266d8167f3b2f73652ef4af
gigisoza <sozashviligigi@gmail.com>
2023-08-31 03:50:40 +0400

    Add hooks in project and change api key

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,51 @@
+import { useState, useEffect } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import arrow from "./images/icon-arrow.svg";
 import background from "./images/pattern-bg-desktop.png";
 import icon from "./icon";
 
-//https://geo.ipify.org/api/v2/country?apiKey=at_3Viw0mpk1UvBQflfHamoLkQ9kVF85&ipAddress=8.8.8.8
+type Address = {
+  ip: string;
+  location: { country: string; region: string; timezone: string };
+  domains: string[];
+  as: {
+    asn: number;
+    name: string;
+    route: string;
+    domain: string;
+    type: string;
+  };
+  isp: string;
+};
 
 function App() {
+  const [address, setAddress] = useState<null | Address>(null);
+  const [ipAddress, setIpAddress] = useState<string>("");
+
+  useEffect(() => {
+    try {
+      const getInitialData = async () => {
+        const res = await fetch(
+          `https://geo.ipify.org/api/v2/country?apiKey=${
+            import.meta.env.VITE_REACT_APP_API_KEY
+          }&ipAddress=192.212.174.101`
+        );
+        const data = await res.json();
+        setAddress(data);
+        console.log(data);
+      };
+
+      getInitialData();
+    } catch (error) {
+      console.trace(error);
+    }
+  }, []);
+
   return (
     <>
       <section>
-        <div className="absolute -z-10">
+        <div className="absolute w-full -z-10">
           <img
             src={background}
             alt="bg-image"
@@ -39,64 +74,68 @@ function App() {
           </form>
         </article>
 
-        <article
-          className="bg-white rounded-lg shadow p-8 mx-8 grid grid-cols-1 gap-8 md:grid-cols-2 
+        {address && (
+          <>
+            <article
+              className="bg-white rounded-lg shadow p-8 mx-8 grid grid-cols-1 gap-8 md:grid-cols-2 
           lg:grid-cols-4 max-w-6xl xl:mx-auto text-center md:text-left lg:-mb-16 relative"
-          style={{ zIndex: 10000 }}
-        >
-          <div className="lg:border-r lg:border-slate-400">
-            <h2 className="uppercase text-sm font-bold text-slate-500 tracking-wider mb-3">
-              Ip Address
-            </h2>
-            <p className="font-semibold text-slate-900 md:text-2xl xl:text-3xl">
-              192.212.174.101
-            </p>
-          </div>
+              style={{ zIndex: 10000 }}
+            >
+              <div className="lg:border-r lg:border-slate-400">
+                <h2 className="uppercase text-sm font-bold text-slate-500 tracking-wider mb-3">
+                  Ip Address
+                </h2>
+                <p className="font-semibold text-slate-900 md:text-2xl xl:text-3xl">
+                  {address.ip}
+                </p>
+              </div>
 
-          <div className="lg:border-r lg:border-slate-400">
-            <h2 className="uppercase text-sm font-bold text-slate-500 tracking-wider mb-3">
-              location
-            </h2>
-            <p className="font-semibold text-slate-900 md:text-2xl xl:text-3xl">
-              Brooklyn
-            </p>
-          </div>
+              <div className="lg:border-r lg:border-slate-400">
+                <h2 className="uppercase text-sm font-bold text-slate-500 tracking-wider mb-3">
+                  location
+                </h2>
+                <p className="font-semibold text-slate-900 md:text-2xl xl:text-3xl">
+                  Brooklyn
+                </p>
+              </div>
 
-          <div className="lg:border-r lg:border-slate-400">
-            <h2 className="uppercase text-sm font-bold text-slate-500 tracking-wider mb-3">
-              Timezone
-            </h2>
-            <p className="font-semibold text-slate-900 md:text-2xl xl:text-3xl">
-              UTC -05:00
-            </p>
-          </div>
+              <div className="lg:border-r lg:border-slate-400">
+                <h2 className="uppercase text-sm font-bold text-slate-500 tracking-wider mb-3">
+                  Timezone
+                </h2>
+                <p className="font-semibold text-slate-900 md:text-2xl xl:text-3xl">
+                  UTC -05:00
+                </p>
+              </div>
 
-          <div>
-            <h2 className="uppercase text-sm font-bold text-slate-500 tracking-wider mb-3">
-              ISP
-            </h2>
-            <p className="font-semibold text-slate-900 md:text-2xl xl:text-3xl">
-              SpaceX
-            </p>
-          </div>
-        </article>
+              <div>
+                <h2 className="uppercase text-sm font-bold text-slate-500 tracking-wider mb-3">
+                  ISP
+                </h2>
+                <p className="font-semibold text-slate-900 md:text-2xl xl:text-3xl">
+                  SpaceX
+                </p>
+              </div>
+            </article>
 
-        <MapContainer
-          center={[51.505, -0.09]}
-          zoom={13}
-          scrollWheelZoom={true}
-          style={{ height: "700px", width: "100vw" }}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker icon={icon} position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
+            <MapContainer
+              center={[51.505, -0.09]}
+              zoom={13}
+              scrollWheelZoom={true}
+              style={{ height: "700px", width: "100vw" }}
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker icon={icon} position={[51.505, -0.09]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
+          </>
+        )}
       </section>
     </>
   );
